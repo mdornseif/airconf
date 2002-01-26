3364e2137d770d268c4727b3e378930efc60ae93
drt <drt@2d70b15c-62de-0310-b45c-a2b26695ab02>
2002-01-26 16:15:27 +0000

    Doc added
    git-svn-id: http://svn.23.nu/svn/repos/airconf/trunk@344 2d70b15c-62de-0310-b45c-a2b26695ab02

--- calcchecksum.py
+++ calcchecksum.py
@@ -1,3 +1,10 @@
+# $Id: calcchecksum.py,v 1.2 2002/01/26 16:15:03 drt Exp $
+
+'''Read Configuration Block from stdin, calculate checksum and write it to stdout.
+
+  --drt@un.bewaff.net - http://c0re.jp/c0de/airconf
+'''
+
 import sys
 import airport
 
--- printconf.py
+++ printconf.py
@@ -1,3 +1,10 @@
+# $Id: printconf.py,v 1.2 2002/01/26 16:15:27 drt Exp $
+
+'''Read Configuration Block from stdin and output it in a human readable Way to stdout.
+
+  --drt@un.bewaff.net - http://c0re.jp/c0de/airconf
+'''
+
 import sys
 import airport
 
