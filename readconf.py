53d27d7487171444b4e94e3b9dbbeb356b0c7233
drt <drt@2d70b15c-62de-0310-b45c-a2b26695ab02>
2002-01-26 16:16:53 +0000

    Doc added, commandline parsing
    git-svn-id: http://svn.23.nu/svn/repos/airconf/trunk@345 2d70b15c-62de-0310-b45c-a2b26695ab02

--- readconf.py
+++ readconf.py
@@ -1,4 +1,15 @@
+# $Id: readconf.py,v 1.2 2002/01/26 16:16:02 drt Exp $
+
+'''Read Configuration from an Airport Basestation and write it to stdout.
+
+  --drt@un.bewaff.net - http://c0re.jp/c0de/airconf
+'''
+
 import sys
 import airport
 
+if len(sys.argv) != 3:
+    print >>sys.stderr, "usage: %s <agent> <community>\ne.g.: %s 172.17.0.1 public" % (sys.argv[0],sys.argv[0])
+    sys.exit(1)
+    
 sys.stdout.write(airport.readconf('172.17.0.8', '2407')) 
--- writeconf.py
+++ writeconf.py
@@ -1,6 +1,17 @@
+# $Id: writeconf.py,v 1.2 2002/01/26 16:16:53 drt Exp $
+
+'''Read Configuration Block from stdin and write it to an Airport Basestation.
+
+  --drt@un.bewaff.net - http://c0re.jp/c0de/airconf
+'''
+
 import sys
 import airport
 
+if len(sys.argv) != 3:
+    print >>sys.stderr, "usage: %s <agent> <community>\ne.g.: %s 172.17.0.1 public" % (sys.argv[0],sys.argv[0])
+    sys.exit(1)
+    
 data = sys.stdin.read() 
 print len(data)
-airport.writeconf('172.17.0.8', '2407', data)
+airport.writeconf(sys.argv[1], sys.argv[2], data)
